42b7e476589cb1a6408d800ec19ae67a4a1c6a82
Dylano Valentijn <58138557+DValentijn@users.noreply.github.com>
2024-04-08 00:50:14 +0200

    Added the profile picture in the navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -25,6 +25,7 @@
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
+                <img src="../storage/{{ Auth::user()->profilepicture }}" style="width: 30px; height: 30px; border-radius: 50px">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
@@ -78,9 +79,10 @@
         </div>
 
         <!-- Responsive Settings Options -->
+
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="px-4">
-                <img src=''></img>
+                <img src="{{ Auth::user()->profilepicture }}" class="w-20 h-20 rounded-full mx-auto">
                 <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
             </div>
